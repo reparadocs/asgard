53817c1d6585ed5ec7f64860f4748072b2b4274d
Rishab Hegde <rishabhegde@Rishabs-MacBook-Pro.local>
2019-10-30 19:36:11 -0700

    cors

--- asgard/settings.py
+++ asgard/settings.py
@@ -55,8 +55,28 @@ MIDDLEWARE = [
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOW_CREDENTIALS = False
+CORS_ALLOW_CREDENTIALS = True
 
+CORS_ALLOW_METHODS = (
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+)
+
+CORS_ALLOW_HEADERS = (
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+)
 
 ROOT_URLCONF = 'asgard.urls'
 
